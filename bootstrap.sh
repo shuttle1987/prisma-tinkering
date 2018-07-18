d6c2a1974bf68d498910637a540746bf84c8a200
Janis Lesinskis <janis@customprogrammingsolutions.com>
2018-07-18 19:25:56 +1000

    Add -y because non-interactive terminal

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,5 @@ echo "deb https://deb.nodesource.com/${NODEREPO} $(lsb_release -sc) main" > /etc
 echo "deb-src https://deb.nodesource.com/${NODEREPO} $(lsb_release -sc) main" >> /etc/apt/sources.list.d/nodesource.list
 sudo apt-get update
 sudo apt-get install -y nodejs
-sudo apt-get install build-essential
+sudo apt-get install -y build-essential
 sudo npm install -g yarn
