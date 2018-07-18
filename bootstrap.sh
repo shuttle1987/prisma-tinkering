549e870ee0bf24a98994676939d1d95e37e2e22a
Janis Lesinskis <janis@customprogrammingsolutions.com>
2018-07-18 19:02:49 +1000

    set up newer nodejs install to work around ubuntu default version being ancient

---
+++ bootstrap.sh
@@ -0,0 +1,6 @@
+NODEREPO="node_10.x"
+curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
+echo 'deb https://deb.nodesource.com/${NODEREPO} $(lsb_release -sc) main' > /etc/apt/sources.list.d/nodesource.list
+echo 'deb-src https://deb.nodesource.com/${NODEREPO} $(lsb_release -sc) main' >> /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
+sudo apt-get install -y nodejs
