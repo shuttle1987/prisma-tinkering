de6949198830d95b3fae4fe7e9631e2cf1c5f6cb
Janis Lesinskis <janis@customprogrammingsolutions.com>
2018-07-18 19:24:56 +1000

    Install needed global packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,5 @@ echo "deb https://deb.nodesource.com/${NODEREPO} $(lsb_release -sc) main" > /etc
 echo "deb-src https://deb.nodesource.com/${NODEREPO} $(lsb_release -sc) main" >> /etc/apt/sources.list.d/nodesource.list
 sudo apt-get update
 sudo apt-get install -y nodejs
+sudo apt-get install build-essential
+sudo npm install -g yarn
